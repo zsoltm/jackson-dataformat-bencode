a8998204bc95a0dc67e4d3398663c7e16c203435
ZsoltM <zsolt@meszarovics.name>
2014-02-25 23:59:41 +0100

    prettified User example type

--- src/test/java/com/fasterxml/jackson/dataformat/bencode/types/User.java
+++ src/test/java/com/fasterxml/jackson/dataformat/bencode/types/User.java
@@ -5,27 +5,27 @@ public class User {
     public enum Gender { MALE, FEMALE }
 
     public static class Name {
-        private String _first, _last;
+        private String first, last;
 
-        public String getFirst() { return _first; }
-        public String getLast() { return _last; }
+        public String getFirst() { return first; }
+        public String getLast() { return last; }
 
-        public void setFirst(String s) { _first = s; }
-        public void setLast(String s) { _last = s; }
+        public void setFirst(String s) { first = s; }
+        public void setLast(String s) { last = s; }
     }
 
-    private Gender _gender;
-    private Name _name;
-    private boolean _isVerified;
-    private byte[] _userImage;
+    private Gender gender;
+    private Name name;
+    private boolean isVerified;
+    private byte[] userImage;
 
-    public Name getName() { return _name; }
-    public boolean isVerified() { return _isVerified; }
-    public Gender getGender() { return _gender; }
-    public byte[] getUserImage() { return _userImage; }
+    public Name getName() { return name; }
+    public boolean isVerified() { return isVerified; }
+    public Gender getGender() { return gender; }
+    public byte[] getUserImage() { return userImage; }
 
-    public void setName(Name n) { _name = n; }
-    public void setVerified(boolean b) { _isVerified = b; }
-    public void setGender(Gender g) { _gender = g; }
-    public void setUserImage(byte[] b) { _userImage = b; }
+    public void setName(Name n) { name = n; }
+    public void setVerified(boolean b) { isVerified = b; }
+    public void setGender(Gender g) { gender = g; }
+    public void setUserImage(byte[] b) { userImage = b; }
 }
